a78c049abf7c67ff47e9264eba096bfd56b0f20c
Robert Bost <bostrt@gmail.com>
2013-03-21 13:48:56 -0400

    urls, added more fields to models, admin site.

---
+++ upastesite/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from upastesite.models import *
+
+admin.site.register(Group)
+admin.site.register(Paste)
--- upastesite/models.py
+++ upastesite/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 class Group(models.Model):
     name = models.TextField()
+    added = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode(self):
+        return self.name
 
 class Paste(models.Model):
     code = models.TextField()
     email = models.EmailField()
     group = models.ForeignKey(Group)
-
-
+    added = models.DateTimeField(auto_now_add=True)
     
+    def __unicode__(self):
+        return self.code
